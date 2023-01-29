951ea9534f14d7baa8cfe1d9a766dde9ac0de71d
Martin <94791875+PomeloPack@users.noreply.github.com>
2023-01-29 15:05:01 +0100

    Add files via upload

--- locators.py
+++ locators.py
@@ -10,14 +10,28 @@ driver = webdriver.Edge(service = service_obj)
 driver.maximize_window()
 driver.get("http://rahulshettyacademy.com/angularpractice/")
 
-# selenum locators: ID, Xpath, CSSSelector, Classname, name, linkText
+# selenum locators: ID, Xpath, CSSSelector, Classname, Name, linkText
+
+# ID, Name select
 driver.find_element(By.NAME, "email").send_keys("hello@gmail.com")
 driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
 
+# CSS tagname[attribute = "value"] -> //input[@type = "submit"] in selectorhub we can usefor id #idname; for classname .classname
+driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Martin")
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio2").click()
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").send_keys(" ")
+driver.find_element(By.XPATH, "(//input[@type='text'])[3]").clear() # to clear the window
+
+
 # XPATH tagname[@attribute = "value"] -> //input[@type = "submit"]
-driver.find_element(By.XPATH, "//input[@type = 'submit']").click
+driver.find_element(By.XPATH, "//input[@type = 'submit']").click()
 message = driver.find_element(By.CLASS_NAME, "alert-success").text # grab the text from class
 print(message)
+assert "Success" in message
 
-time.sleep(100)
+
+
+
+
+time.sleep(5000)
